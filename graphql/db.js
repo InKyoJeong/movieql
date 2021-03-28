2d33bee9c3d674bf54ab9dec79604ef9fb10d610
InKyoJeong <viapolar@naver.com>
2021-03-29 01:46:21 +0900

    Wrap a REST API with grapghql

--- graphql/db.js
+++ graphql/db.js
@@ -1,34 +1,9 @@
-let movies = [
-  { id: 0, name: "Taeyeon", score: 3 },
-  { id: 1, name: "Zero", score: 1 },
-  { id: 2, name: "Nero", score: 5 },
-  { id: 3, name: "Ginger", score: 4 },
-  { id: 4, name: "Boogi", score: 3 },
-];
+import fetch from "node-fetch";
 
-export const getMovies = () => movies;
+const API_URL = "https://yts.mx/api/v2/list_movies.json";
 
-export const getById = (id) => {
-  const filteredMovies = movies.filter((movie) => id === movie.id);
-  return filteredMovies[0];
-};
-
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter((movie) => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
-  }
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
+export const getMovies = (limit, rating) => {
+  fetch(`${API_URL}`)
+    .then((res) => res.json())
+    .then((json) => json.data.movies);
 };
--- readme.md
+++ readme.md
@@ -4,10 +4,15 @@
 $ yarn init
 $ yarn add graphql-yoga
 $ yarn add @babel/cli @babel/core @babel/node @babel/preset-env
+$ yarn add node-fetch
 ```
 
 - `Query` : 데이터베이스로 요청하여 받을 데이터. get data (Read)
 - `Mutation` : 서버, 데이터베이스, 메모리의 데이터 변경 작업. mutate data. (Create, Upadate, Delete)
+
+## Movie API
+
+- [https://yts.mx/api/v2/list_movies.json](https://yts.mx/api/v2/list_movies.json)
 
 ## delete
 
